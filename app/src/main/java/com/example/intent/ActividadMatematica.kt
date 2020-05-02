23283470ea49ef06588e339090f35e8641ee868b
Celtor123 <celsosan11@gmail.com>
2020-05-03 01:32:05 +0200

    Reto web hecho, completando el matemático.

---
+++ app/src/main/java/com/example/intent/ActividadMatematica.kt
@@ -0,0 +1,14 @@
+package com.example.intent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.browse
+
+class ActividadMatematica : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
--- app/src/main/java/com/example/intent/MainActivity.kt
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -2,15 +2,17 @@ package com.example.intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
-/*private val dos=boton2
-private val tres=boton3
-private val cuatro=boton4*/
+    /*private val dos=boton2
+    private val tres=boton3
+    private val cuatro=boton4*/
+    //Función que llama al xml al iniciar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,18 +24,24 @@ private val cuatro=boton4*/
         val btn2: Button = findViewById(R.id.boton2)
         val btn3: Button = findViewById(R.id.boton3)
         val btn4: Button = findViewById(R.id.boton4)
+
         btn1.onClick {
-            toast("Uno")
+            Log.d("Web","activado")
+            browse("https://www.google.es")
         }
+
         btn2.onClick {
-            toast("Dos")
+            Log.d("camara","activado")
         }
+
         btn3.onClick {
-            toast("Tres")
+            Log.d("mate","activado")
+            startActivity<ActividadMatematica>("id" to 5)
         }
 
         btn4.onClick {
-            toast("Cuatro")
+            Log.d("nuevo","activado")
+
         }
 
     }
