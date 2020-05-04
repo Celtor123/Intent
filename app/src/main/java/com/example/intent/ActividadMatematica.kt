be6f207b0484d47b5d32653bcdf7000db7189cff
Celtor123 <celsosan11@gmail.com>
2020-05-04 23:51:05 +0200

    Añadido reto matmático completado.

--- app/src/main/java/com/example/intent/ActividadMatematica.kt
+++ app/src/main/java/com/example/intent/ActividadMatematica.kt
@@ -1,14 +1,90 @@
 package com.example.intent
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import org.jetbrains.anko.browse
+import kotlinx.android.synthetic.main.actividadsecundaria.view.*
+import org.jetbrains.anko.db.INTEGER
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.toast
+import org.w3c.dom.Text
 
-class ActividadMatematica : AppCompatActivity() {
+open class ActividadMatematica : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.actividadsecundaria)
+    }
+
+
+
+
+
+    override fun onResume() {
+        super.onResume()
+        a()
+        }
+
+    fun a(){
+        val btn1: TextView = findViewById (R.id.num1)
+        val btn2: TextView = findViewById (R.id.num2)
+        val operacion:TextView=findViewById(R.id.oper)
+        val restant:EditText?=findViewById(R.id.entrnum)
+        val btnok: Button = findViewById(R.id.btnok)
+        var resultado:Int? = 0
+
+        var uno=(0..100).random()
+        btn1.text= uno.toString()
+        val dos=(0..100).random()
+        btn2.text=dos.toString()
+        val algo=(1..4).random()
+        val list = listOf("+","-","x","/")
+        val lista = list.get(algo)
+        operacion.text=lista
+
+        if(lista.equals("+")){
+            resultado=uno+dos
+        }
+        else if(lista.equals("-")){
+            if (uno>=dos){
+                resultado=uno-dos
+            }
+            else{
+                while(uno<dos){
+                    uno = (0..100).random()
+                }
+                resultado=uno-dos
+            }
+
+        }
+        else if(lista.equals("x")){
+            resultado=uno*dos
+        }
+        else if(lista.equals("/")){
+            resultado=uno/dos
+        }
+
+
+        btnok.onClick {
+
+            val s = Integer.parseInt(restant!!.getText().toString())
+            if(resultado?.equals(s)!!){
+                toast("Resultado Correcto")
+                a()
+            }else{
+                toast("No es correcto el resultado")
+            }
+        }
+    }
+
+
+
+
 
 
     }
-}
+
--- app/src/main/java/com/example/intent/MainActivity.kt
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -9,34 +9,39 @@ import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
-    /*private val dos=boton2
-    private val tres=boton3
-    private val cuatro=boton4*/
     //Función que llama al xml al iniciar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    /*
+    Función que crea botones, los incializa a través de la id del xml
+    y a través de sus pulsaciones inicia los retos
+     */
     override fun onResume() {
         super.onResume()
         val btn1: Button = findViewById (R.id.boton1)
         val btn2: Button = findViewById(R.id.boton2)
         val btn3: Button = findViewById(R.id.boton3)
         val btn4: Button = findViewById(R.id.boton4)
-
+        /*
+        onClick para interacción al pulsar botones
+        browse
+         */
         btn1.onClick {
             Log.d("Web","activado")
             browse("https://www.google.es")
         }
 
         btn2.onClick {
-            Log.d("camara","activado")
+            Log.d("mate","activado")
+            startActivity<ActividadMatematica>()
         }
 
         btn3.onClick {
-            Log.d("mate","activado")
-            startActivity<ActividadMatematica>("id" to 5)
+            Log.d("camara","activado")
+
         }
 
         btn4.onClick {
